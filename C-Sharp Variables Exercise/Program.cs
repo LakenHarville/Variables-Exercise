d1af656e59dbcd3b269627ce47df07fd6d832ce1
Richard Laken Harville <rlakenharville@gmail.com>
2021-09-10 22:45:02 -0500

    initial commit

---
+++ C-Sharp Variables Exercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C_Sharp_Variables_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogAge = 10;
+            string dogName = "Ralph";
+            bool isAwesome = true;
+            char one = '1';
+            double dbl = 5.5;
+            decimal dec = 5.45M;
+            Console.WriteLine($@"My dog's name is {dogName},
+He is {dogAge} years old, I have had him for {one} year. I want to keep him for {dbl} more years,
+but I will likely only keep him for {dec} years."); 
+        }
+    }
+}
